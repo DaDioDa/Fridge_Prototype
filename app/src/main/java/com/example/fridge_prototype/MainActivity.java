011cd839de965cb0dcbfcf3196044dcf20fde115
DaDioDa <56485296+DaDioDa@users.noreply.github.com>
2022-05-11 04:30:26 +0800

    test

--- app/src/main/java/com/example/fridge_prototype/MainActivity.java
+++ app/src/main/java/com/example/fridge_prototype/MainActivity.java
@@ -3,6 +3,7 @@ package com.example.fridge_prototype;
 import androidx.appcompat.app.AppCompatActivity;
 
 import android.os.Bundle;
+import android.util.Log;
 
 public class MainActivity extends AppCompatActivity {
 
@@ -11,6 +12,6 @@ public class MainActivity extends AppCompatActivity {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
         //git test 123
-        //qwer
+        Log.d("test","Debug test");
     }
 }
